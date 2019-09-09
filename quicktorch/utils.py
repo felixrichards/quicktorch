b8a693f85d1085a3fada904c16f438fcc72d97dd
Felix Richards <felixarichards@gmail.com>
2019-09-09 13:55:37 +0100

    grad flow plotting function

--- quicktorch/utils.py
+++ quicktorch/utils.py
@@ -9,12 +9,16 @@ from .vis import TrainPlot
 from sklearn.model_selection import KFold
 from skimage import io
 import PIL
+from matplotlib.lines import Line2D
 """This module provides miscellaneous helper functions for neural network
 experimentation, most notably a training function.
 """
+counter = 0
+limit = 6
 
 
-def perform_pass(net, data, opt, criterion, device, train=True):
+def perform_pass(net, data, opt, criterion, device, train=True,
+                 epoch=None, i=None):
     """Performs a forward and backward pass.
 
     Args:
@@ -41,9 +45,43 @@ def perform_pass(net, data, opt, criterion, device, train=True):
     start = time.time()
     if train:
         loss.backward()
+        plot_grad_flow(net.named_parameters(), epoch, i)
         opt.step()
         # print("Backward pass done in", time.time() - start)
     return loss, output
+
+
+def plot_grad_flow(named_parameters, epoch, i):
+    '''Plots the gradients flowing through different layers in the net during training.
+    Can be used for checking for possible gradient vanishing / exploding problems.
+
+    Usage: Plug this function in Trainer class after loss.backwards() as 
+    "plot_grad_flow(self.model.named_parameters())" to visualize the gradient flow'''
+    ave_grads = []
+    max_grads = []
+    layers = []
+    for n, p in named_parameters:
+        if(p.requires_grad) and ("bias" not in n):
+            layers.append(n)
+            ave_grads.append(p.grad.abs().mean())
+            max_grads.append(p.grad.abs().max())
+    plt.bar(np.arange(len(max_grads)), max_grads, alpha=0.1, lw=1, color="c")
+    plt.bar(np.arange(len(max_grads)), ave_grads, alpha=0.1, lw=1, color="b")
+    plt.hlines(0, 0, len(ave_grads)+1, lw=2, color="k")
+    plt.xticks(range(0, len(ave_grads), 1), layers, rotation="vertical")
+    plt.xlim(left=0, right=len(ave_grads))
+    plt.ylim(bottom=-0.001, top=1)  # zoom in on the lower gradient regions
+    plt.xlabel("Layers")
+    plt.ylabel("average gradient")
+    plt.title("Gradient flow")
+    plt.grid(True)
+    plt.legend([Line2D([0], [0], color="c", lw=4),
+                Line2D([0], [0], color="b", lw=4),
+                Line2D([0], [0], color="k", lw=4)], ['max-gradient', 'mean-gradient', 'zero-gradient'])
+    plt.tight_layout()
+    fname = "figs/gradflow_epoch{}_iter{}".format(epoch, i)
+    plt.savefig(fname)
+    print("gradflow saved under", fname)
 
 
 def train(net, input, criterion='default',
@@ -153,7 +191,8 @@ def train(net, input, criterion='default',
                 start = time.time()
                 loss, output = perform_pass(net, data, opt,
                                             criterion, device,
-                                            phase == 'train')
+                                            phase == 'train',
+                                            epoch=epoch, i=i)
                 # print("Full pass done in", time.time() - start)
                 start = time.time()
 
@@ -175,9 +214,10 @@ def train(net, input, criterion='default',
                 # Print progress
                 if i % print_iter == print_iter - 1:
                     print('Epoch [{}/{}]. Iter [{}/{}]. Loss: {:.4f}. '
-                          'Acc: {:.4f}. Avg time/iter: {:.4f}. '
+                          'Acc: {:.4f}. '
                           'Precision: {:.4f}. '
                           'Recall: {:.4f}. '
+                          'Avg time/iter: {:.4f}. '
                           .format(
                             epoch+1, epochs, i, size[phase]//b_size['train'],
                             running_loss/((i+1)*b_size[phase]),
