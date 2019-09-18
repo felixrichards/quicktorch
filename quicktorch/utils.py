24eb38ec6597f8edf9fd3eaa44bc33b15219d5b0
Felix Richards <felixarichards@gmail.com>
2019-09-18 13:40:13 +0100

    PSNR for mask targets

--- quicktorch/utils.py
+++ quicktorch/utils.py
@@ -169,7 +169,7 @@ def train(net, input, criterion='default',
                         print(10 * log10(1 / loss.item()))
                         accuracy = ((i * accuracy + 10 * log10(1 / loss.item()) *
                                      data[0].size(0) / b_size[phase]) /
-                                    i + 1)
+                                    (i + 1))
                 else:
                     out_idx = output.max(dim=1)[1]
                     lbl_idx = data[1].max(dim=1)[1]
