319a657cc84614ccf55168a764b3a8c0082e8ac9
Felix Richards <felixarichards@gmail.com>
2021-12-16 16:15:19 +0000

    Consensus loss now handles seg_criterion. Focal loss

--- quicktorch/modules/attention/loss.py
+++ quicktorch/modules/attention/loss.py
@@ -1,88 +1,189 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 from quicktorch.modules.loss import ConsensusLossMC
+
+
+class GuidedAuxLoss(nn.Module):
+    def __init__(self, criterion=nn.MSELoss()):
+        super().__init__()
+        self.criterion = criterion
+
+    def forward(self, aux_outputs):
+        guided_losses, reconstruction_losses = zip(*[self.aux_guided_loss(aux) for aux in aux_outputs])
+        return .25 * sum(guided_losses) + .1 * sum(reconstruction_losses)
+
+    def aux_guided_loss(self, aux):
+        return (
+            self.criterion(aux['in_semantic_vectors'], aux['out_semantic_vectors']),
+            self.criterion(aux['in_attention_encodings'], aux['out_attention_encodings'])
+        )
 
 
 class DAFLoss(nn.Module):
     def __init__(self,
                  seg_criterion=nn.BCEWithLogitsLoss(),
-                 vec_criterion=nn.MSELoss(),
-                 pos_weight=7):
+                 aux_loss=GuidedAuxLoss(),
+                 pos_weight=None):
         super().__init__()
         self.seg_criterion = seg_criterion
-        self.seg_criterion.pos_weight = torch.tensor(pos_weight)
-        self.vec_criterion = vec_criterion
+        if pos_weight is not None:
+            self.seg_criterion.pos_weight = pos_weight
+        self.aux_loss = aux_loss
 
     def forward(self, output, target):
         if not (type(output[0]) == tuple or type(output[0]) == list):
             return self.seg_criterion(output, target)
-        (
-            first_attention_vectors,
-            second_attention_vectors,
-            attention_in_encodings,
-            attention_out_encodings,
-            segmentations
-        ) = output
-        guided_losses = [
-            self.vec_criterion(pre_a, a)
-            for pre_a, a in zip(first_attention_vectors, second_attention_vectors)
-        ]
-        reconstruction_losses = [
-            self.vec_criterion(in_enc, out_enc)
-            for in_enc, out_enc in zip(attention_in_encodings, attention_out_encodings)
-        ]
+
+        segmentations, aux_outputs = output
+
         seg_losses = [
             self.seg_criterion(seg, target)
             for seg in segmentations
         ]
-        return sum(seg_losses) + .25 * sum(guided_losses) + .1 * sum(reconstruction_losses)
+        aux_loss = self.aux_loss(aux_outputs)
+        return sum(seg_losses) + aux_loss
 
 
-class DAFLossMCML(ConsensusLossMC):
+class DAFConsensusLoss(DAFLoss):
     """Loss class for DAF network on multiclass multilabel
 
     Args:
-        eta (float, optional): consensus majority value. Defaults to 0.4 to
-            compensate for FP errors.
-        lambda (float, optional): boosts negative class weighting. Defaults to 1.1.
         seg_criterion (nn._loss, optional): loss fn used for segmentation
             labels.
-        vec_criterion (nn._loss, optional): loss fn used for attention
+        aux_loss (nn._loss, optional): loss fn used for attention
             vectors in self-guided protocol.
     """
-    def __init__(self, eta=.4, lambd=1.1, beta=None,
-                 seg_criterion=nn.BCEWithLogitsLoss(reduction='none'),
-                 vec_criterion=nn.MSELoss()):
-        super().__init__(eta=eta, lambd=lambd, beta=beta)
-        self.seg_criterion = seg_criterion
-        self.vec_criterion = vec_criterion
+    def __init__(self,
+                 consensus_criterion=ConsensusLossMC(eta=.4, lambd=1.1, beta=None),
+                 aux_loss=GuidedAuxLoss(),
+                 pos_weight=None):
+        super().__init__(
+            seg_criterion=consensus_criterion,
+            aux_loss=aux_loss,
+            pos_weight=pos_weight
+        )
+
+
+class FocalWithLogitsLoss(nn.Module):
+    def __init__(self, pos_weight=2., gamma=2., reduction='none'):
+        super().__init__()
+        print(f"in focalloss {pos_weight=}")
+        self.alpha = 1 / pos_weight
+        self.gamma = gamma
+        self.eps = 1e-6
+        self.reduction = reduction
 
     def forward(self, output, target):
-        target[target >= self.eta] = 1.
-        weight = self.loss_weight(target)
+        prob = torch.sigmoid(output).clip(self.eps, 1. - self.eps)
 
-        if not (type(output[0]) == tuple or type(output[0]) == list):
-            return (self.seg_criterion(output, target) * weight).mean()
-        (
-            first_attention_vectors,
-            second_attention_vectors,
-            attention_in_encodings,
-            attention_out_encodings,
-            segmentations
-        ) = output
-        # print([seg.mean(dim=(0, 2, 3)) for seg in segmentations])
-        # print(target.mean(dim=(0, 2, 3)))
-        guided_losses = [
-            self.vec_criterion(pre_a, a)
-            for pre_a, a in zip(first_attention_vectors, second_attention_vectors)
-        ]
-        reconstruction_losses = [
-            self.vec_criterion(in_enc, out_enc)
-            for in_enc, out_enc in zip(attention_in_encodings, attention_out_encodings)
-        ]
-        seg_losses = [
-            (self.seg_criterion(seg, target) * weight).mean()
-            for seg in segmentations
-        ]
-        return sum(seg_losses) + .25 * sum(guided_losses) + .1 * sum(reconstruction_losses)
+        # not perfect, consider reimplementing with log_softmax for numerical stability
+        back_ce = -(1 - target) * torch.pow(prob, self.gamma) * torch.log(1 - prob)
+        fore_ce = -target * torch.pow(1 - prob, self.gamma) * torch.log(prob)
+
+        out = (1 - self.alpha) * back_ce + self.alpha * fore_ce
+        if self.reduction == 'none':
+            return out
+        return out.mean()
+
+
+class ClassBalancedBCEWithLogitsLoss(nn.Module):
+    def __init__(self, pos_weight=None, beta=.9999, reduction='none'):
+        super().__init__()
+        self.pos_weight = pos_weight
+        self.n_classes = pos_weight.shape[0]
+        self.beta = beta
+        self.reduction = reduction
+
+    def forward(self, output, target):
+        """Compute the Class Balanced Loss between `output` and the ground truth `target`.
+
+        Class Balanced Loss: ((1 - beta) / (1 - beta ^ n)) * L(target, output)
+        where L is one of the standard losses used for Neural Networks.
+        Args:
+        output: A float tensor of size [batch, self.n_classes].
+        target: A int tensor of size [batch, self.n_classes].
+        samples_per_cls: A python list of size [self.n_classes].
+        self.n_classes: total number of classes. int
+        loss_type: string. One of "sigmoid", "focal", "softmax".
+        beta: float. Hyperparameter for Class balanced loss.
+        Returns:
+        cb_loss: A float tensor representing class balanced loss
+        """
+        effective_num = 1.0 - torch.pow(self.beta, self.pos_weight)
+        weights = (1.0 - self.beta) / effective_num
+        weights = weights / torch.sum(weights) * self.n_classes  # [c]
+
+        weights = weights.view(1, self.n_classes, 1, 1) # [1, c] [1, c, w, h]
+        weights = weights.repeat(target.shape[0], 1, 1, 1) * target  # [n, c] * [n, c]  [n, c, w, h]
+        weights = weights.sum(1, keepdim=True) # [n, 1, w, h]
+        weights = weights.repeat(1, self.n_classes, 1, 1) # [n, c, w, h]
+
+        return F.binary_cross_entropy_with_logits(input=output, target=target, weight=weights, reduction=self.reduction)
+
+
+class AsymmetricFocalTvesrkyWithLogitsLoss(nn.Module):
+    def __init__(self, pos_weight=None, delta=0.6, gamma=0.2, reduction='none'):
+        super().__init__()
+        self.delta = delta
+        self.gamma = gamma
+        self.reduction = reduction
+        self.eps = 1e-8
+
+    def forward(self, output, target):
+        # Clip values to prevent division by zero error
+        output = torch.sigmoid(output).clip(self.eps, 1. - self.eps)
+
+        # Calculate true positives (tp), false negatives (fn) and false positives (fp)     
+        axis = (-1, -2)
+        tp = torch.sum(target * output, axis=axis)
+        tn = torch.sum((1 - target) * (1 - output), axis=axis)
+        fn = torch.sum(target * (1 - output), axis=axis)
+        fp = torch.sum((1 - target) * output, axis=axis)
+        dice_class_pos = (tp + self.eps) / (tp + self.delta * fn + (1 - self.delta) * fp + self.eps)
+        dice_class_neg = (tn + self.eps) / (tn + self.delta * fp + (1 - self.delta) * fn + self.eps)
+        # Calculate losses separately for each class, only enhancing foreground class
+        back_dice = (1 - dice_class_neg)
+        fore_dice = (1 - dice_class_pos) * torch.pow(1 - dice_class_pos, -self.gamma)
+
+        loss = back_dice + fore_dice
+
+        return loss.unsqueeze(-1).unsqueeze(-1)
+
+
+class UnifiedFocalWithLogitsLoss(nn.Module):
+    def __init__(self, weight=0.5, pos_weight=None, delta=0.6, gamma=0.2, reduction='none'):
+        super().__init__()
+        self.weight = weight
+        self.delta = delta
+        self.gamma = gamma
+        self.reduction = reduction
+        self.aft_loss = AsymmetricFocalTvesrkyWithLogitsLoss(
+            delta=delta,
+            gamma=gamma,
+            reduction=reduction
+        )
+        self.eps = 1e-8
+
+    def forward(self, output, target):
+        return (
+            self.weight * self.aft_loss(output, target)
+            # ((1 - self.weight) * self.asymmetric_focal_loss(output, target))
+        )
+
+    def asymmetric_focal_loss(self, output, target):
+        # output = torch.sigmoid(output).clip(self.eps, 1. - self.eps)
+
+        # calculate losses separately for each class, only suppressing background class
+        back_ce = -(1 - target) * torch.pow(1 - torch.sigmoid(output).clip(self.eps, 1. - self.eps), self.gamma) * torch.log(1 - torch.sigmoid(output).clip(self.eps, 1. - self.eps))
+
+        fore_ce = -target * F.logsigmoid(output)
+
+        # focal_factor = (1 - target).mul_(torch.pow(1 - torch.sigmoid(output).clip(self.eps, 1. - self.eps), self.gamma))
+        # loss = (output * focal_factor).add_((-output).exp() + 1).mul_()
+        # loss = (-output).exp_().add_(1).log_().mul_(focal_factor + target).add_(x * focal_factor)
+
+        # loss = (1 - target).mul_((1 - output).pow_(self.gamma)).mul_(torch.log(torch.sigmoid(output).mul_(-1).add_(1))).add_(target.mul_(F.logsigmoid(output)))
+
+        return (1 - self.delta) * back_ce + self.delta * fore_ce
--- quicktorch/modules/loss.py
+++ quicktorch/modules/loss.py
@@ -3,17 +3,21 @@ import torch.nn as nn
 
 
 class ConsensusLoss(nn.Module):
-    def __init__(self, eta=.4, lambd=1.1, beta=None):
+    def __init__(self, eta=.4, lambd=1.1, beta=None, pos_weight=None,
+                 seg_criterion=torch.nn.BCEWithLogitsLoss(reduction='none')):
         super().__init__()
         self.eta = eta
         self.beta = beta
         self.lambd = lambd
-        self.seg_criterion = torch.nn.BCEWithLogitsLoss(reduction='none')
+        self.seg_criterion = seg_criterion
+        if pos_weight is not None:
+            self.seg_criterion.pos_weight = pos_weight
 
     def forward(self, output, target):
-        target[target >= self.eta] = 1.
         weight = self.loss_weight(target)
-        return (self.seg_criterion(output, target) * weight).mean()  
+        target[target >= self.eta] = 1.
+        target[target < self.eta] = 0.
+        return (self.seg_criterion(output, target) * weight).mean()
 
     def loss_weight(self, target):
         p = torch.sum(target >= self.eta)
@@ -24,22 +28,41 @@ class ConsensusLoss(nn.Module):
         return weight
 
 
-class ConsensusLossMC(ConsensusLoss):
-    def __init__(self, eta=.4, lambd=1.1, beta=None):
-        super().__init__(eta=eta, lambd=lambd, beta=beta)
+class PlainConsensusLossMC(ConsensusLoss):
+    def __init__(self, eta=.49, **kwargs):
+        super().__init__(eta=eta, **kwargs)
 
     def loss_weight(self, target):
-        p = torch.sum(target >= self.eta, dim=(0, 2, 3))
-        n = torch.sum(target == 0, dim=(0, 2, 3))
-        pos_weight = (n / (p + n))[None, :, None, None]
-        neg_weight = (self.lambd * p / (p + n))[None, :, None, None]
-        pos_weight = pos_weight.expand_as(target) * (target >= self.eta)
-        neg_weight = neg_weight.expand_as(target) * (target == 0)
+        weight = torch.tensor([1], device=target.device)[None, :, None, None]
+        weight = weight.expand_as(target)
 
-        # if self.beta is not None:
-        #     w = torch.sum(0 < target < self.beta, dim=(0, 2, 3))
-        #     weak_weight = (self.lambd * p / (p + n))[None, :, None, None]
-        #     weak_weight = weak_weight.expand_as(target) * (target == 0)
+        return weight
 
-        weight = pos_weight + neg_weight
+
+class ConsensusLossMC(ConsensusLoss):
+    def __init__(self, eta=.49, **kwargs):
+        super().__init__(eta=eta, **kwargs)
+
+    def loss_weight(self, target):
+        weight = torch.tensor([1], device=target.device)[None, :, None, None]
+        weight = weight.expand_as(target) * (target == 0. + 0.49 < target)
+
+        return weight
+
+
+class SuperMajorityConsensusLossMC(ConsensusLoss):
+    def __init__(self, eta=.49, sm=.74, beta=1.25, **kwargs):
+        super().__init__(eta=eta, beta=beta, **kwargs)
+        self.sm = sm
+
+    def loss_weight(self, target):
+        super_maj_weight = torch.tensor([self.beta], device=target.device)[None, :, None, None]
+        maj_weight = torch.tensor([1], device=target.device)[None, :, None, None]
+        super_maj_weight = super_maj_weight.expand_as(target) * (target >= self.sm)
+        target <= 0.24
+        target >= self.eta
+        target < self.sm
+        maj_weight = maj_weight.expand_as(target) * (target <= 0.24 + torch.logical_and(target >= self.eta, target < self.sm))
+
+        weight = super_maj_weight + maj_weight
         return weight
